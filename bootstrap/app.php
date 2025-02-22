ae0213f83f9feb53a4f1dafa6ffff0ab247f7a57
r-danialf <rafiedanialfathurrahman@gmail.com>
2025-02-22 19:36:23 +0700

    SCRATCH REWRITE!!!

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\AdminMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,9 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias([
-            'admin' => AdminMiddleware::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
